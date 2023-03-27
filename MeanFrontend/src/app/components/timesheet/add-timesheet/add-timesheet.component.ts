a32ec04760f201280ddab8df7612c031e2fdcbe8
swati <cypress.automationproject@gmail.com>
2023-03-27 12:02:27 +0530

    Add Timesheet

--- MeanFrontend/src/app/components/timesheet/add-timesheet/add-timesheet.component.ts
+++ MeanFrontend/src/app/components/timesheet/add-timesheet/add-timesheet.component.ts
@@ -4,6 +4,9 @@ import { TimesheetService } from 'src/app/shared/timesheet.service';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
 import { TaskService } from 'src/app/shared/task.service';
+import { ProjectService } from 'src/app/shared/project.service';
+import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @Component({
@@ -12,53 +15,69 @@ import { TaskService } from 'src/app/shared/task.service';
   styleUrls: ['./add-timesheet.component.scss']
 })
 export class AddTimesheetComponent implements OnInit {
- 
+
   public timesheet: Timesheet = new Timesheet;
-  public tasks:any  = []
+  public tasks: any = []
+  public projects: any = []
   public selectedLevel = ''
-    
+  selectedDate!: NgbDate;
+
   constructor(private timesheetService: TimesheetService,
     private toastr: ToastrService,
     private router: Router,
-    private taskService: TaskService) { }
+    private taskService: TaskService,
+    private projectService: ProjectService) { }
 
   ngOnInit(): void {
+    this.getProject()
     this.getTask()
+  
   }
 
   showSuccess(message: string) {
     this.toastr.success(message)
   }
-  
-  getTask(){
-    this.taskService.readTask().subscribe(
+
+  getProject() {
+    this.projectService.readProject().subscribe(
       data => {
-        this.tasks=data
+        this.projects = data
       },
       error => {
         console.log(error);
-        
+
+      }
+    )
+  }
+  getTask() {
+    this.taskService.readTask().subscribe(
+      data => {
+        this.tasks = data
+      },
+      error => {
+        console.log(error);
+
       }
     )
   }
 
-    addTimesheet() {
-      this.timesheetService.addTimesheet(this.timesheet).subscribe(
-        data => {
-          console.log(data);
-          this.router.navigate(['/addTimesheet']);
-          this.showSuccess('Timesheet Added Successfully')
-        },
-        error => {
-          console.log(error);
+  addTimesheet() {
+    this.timesheetService.addTimesheet(this.timesheet).subscribe(
+      data => {
+        console.log(data);
+        this.router.navigate(['/addTimesheet']);
+        this.showSuccess('Timesheet Added Successfully')
+      },
+      error => {
+        console.log(error);
 
-        }
-      )
-    }
+      }
+    )
+  }
 
-    selected(){
-      console.log(this.selectedLevel)
-    }
+  selected() {
+    console.log(this.selectedLevel)
+  }
 
 
 
